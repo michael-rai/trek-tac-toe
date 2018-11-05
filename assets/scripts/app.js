b46385405d02a60f2d1c0d2c031ae934c2b8e35a
michael-rai <michaelrai.prgmr@gmail.com>
2018-11-05 07:56:11 -0500

    Final Commit before project submission
    Linter errors heavily delayed the my process. must write function for
    oppTurn. No API connectivity, UI missing transition from click of close
    opted to not go with a clear button rather wanted to incorporate that
    as a function win a win is made.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,32 +8,63 @@ $(() => {
   console.log('js is running')
   //  $('#signUpFormModal').on('submit', events.onSignUp)
   //  $('#loginInFormModal').on('submit', events.onSignIn)
+
+  // the following code can be attributed to Cody Seibert via Youtube at https://www.youtube.com/watch?v=ra2_rKV0mDE
   const grid = [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' ']
   ]
-  $('.container').hide()
-
-  // const isGameOver = function () {
-  // // check if game is overflow
-  // for (var i = 0; i < 3, i++) {
-  //   if (grid[i][0]) !== ' ' &&
-  // }
-
+  //    const isGameOver = function () {
+  //      // check horizontal
+  //      for (var i = 0; i < 3; i++){
+  //        if (grid[i][0] !== ' ' &&
+  //        grid[i][0] === grid [i][2] &&
+  //        grid[i][0] === grid [i][2]) {
+  //         return grid [i][0]
+  //
+  //       //check vertical
+  //       for (var i = 0; i < 3; i++){
+  //         if (grid[0][j] !== ' ' &&
+  //         grid[0][j] === grid [1][j] &&
+  //         grid[0][j] === grid [2][j]) {
+  //          return grid [0][j]
+  //        }
+  //      }
+  //
+  //        // check diagonal-top left bottom right
+  //          if (grid[0][0] !== ' ' &&
+  //          grid[0][0] === grid [1][1] &&
+  //          grid[0][0] === grid [2][2]) {
+  //           return grid [0][0]
+  //         }
+  //
+  //         //check diagonal-bottom left top right
+  //         if (grid[2][0] !== ' ' &&
+  //         grid[2][0] === grid [1][1] &&
+  //         grid[2][0] === grid [0][2]) {
+  //          return grid [2][0]
+  //        }
+  //
+  //        for (var i = 0; i < 3; i++) {
+  //          for (var i = 0; i < 3; i++){
+  //            if (grid[i][j] === ' ') {
+  //              return false
+  //          }
+  // }}
+  //        return null;
+  //
   $('.col').click(function () {
     $(this).html(userToken)
     const i = $(this).data('i')
     const j = $(this).data('j')
     grid[i][j] = userToken
-
-    // if (isGameOver()) {
-    //
-    // } else {
-    //   // opp turn
-    // }
-    $('#testToggle').close(function () {
-      $('.container').toggle()
-    })
   })
+//     if (isGameOver()) {
+//
+//      } else {
+//        oppTurn
+//
+//   //  $('#testToggle').close(function () {
+//   //    $('.container').toggle()
 })
