ea2d8215b5ade18044ea14722aa442333f916096
michael-rai <michaelrai.prgmr@gmail.com>
2018-11-18 17:50:13 -0500

    began AI logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 // const events = require('./events')
 
 const userToken = 'X'
-// const oppToken = 'Y'
+const oppToken = 'Y'
 
 $(() => {
   console.log('js is running')
@@ -16,56 +16,83 @@ $(() => {
     [' ', ' ', ' '],
     [' ', ' ', ' ']
   ]
-  //    const isGameOver = function () {
-  //      // check horizontal
-  //      for (var i = 0; i < 3; i++){
-  //        if (grid[i][0] !== ' ' &&
-  //        grid[i][0] === grid [i][2] &&
-  //        grid[i][0] === grid [i][2]) {
-  //         return grid [i][0]
-  //
-  //       //check vertical
-  //       for (var i = 0; i < 3; i++){
-  //         if (grid[0][j] !== ' ' &&
-  //         grid[0][j] === grid [1][j] &&
-  //         grid[0][j] === grid [2][j]) {
-  //          return grid [0][j]
-  //        }
-  //      }
-  //
-  //        // check diagonal-top left bottom right
-  //          if (grid[0][0] !== ' ' &&
-  //          grid[0][0] === grid [1][1] &&
-  //          grid[0][0] === grid [2][2]) {
-  //           return grid [0][0]
-  //         }
-  //
-  //         //check diagonal-bottom left top right
-  //         if (grid[2][0] !== ' ' &&
-  //         grid[2][0] === grid [1][1] &&
-  //         grid[2][0] === grid [0][2]) {
-  //          return grid [2][0]
-  //        }
-  //
-  //        for (var i = 0; i < 3; i++) {
-  //          for (var i = 0; i < 3; i++){
-  //            if (grid[i][j] === ' ') {
-  //              return false
-  //          }
-  // }}
-  //        return null;
-  //
-  $('.col').click(function () {
-    $(this).html(userToken)
-    const i = $(this).data('i')
-    const j = $(this).data('j')
-    grid[i][j] = userToken
-  })
-//     if (isGameOver()) {
-//
-//      } else {
-//        oppTurn
-//
-//   //  $('#testToggle').close(function () {
-//   //    $('.container').toggle()
+  const isGameOver = function () {
+    // check horizontal
+    for (let i = 0; i < 3; i++) {
+      if (grid[i][0] !== ' ' &&
+         grid[i][0] === grid[i][1] &&
+         grid[i][0] === grid[i][2]) {
+        // return grid[i][0]
+
+        // check vertical
+        for (let j = 0; j < 3; j++) {
+          if (grid[0][j] !== ' ' &&
+          grid[0][j] === grid [1][j] &&
+          grid[0][j] === grid [2][j]) {
+           // return grid [0][j]
+        //  }
+    //   }
+
+         // check diagonal-top left bottom right
+           if (grid[0][0] !== ' ' &&
+           grid[0][0] === grid [1][1] &&
+           grid[0][0] === grid [2][2]) {
+      //      return grid [0][0]
+          }
+
+          //check diagonal-bottom left top right
+          if (grid[2][0] !== ' ' &&
+          grid[2][0] === grid [1][1] &&
+          grid[2][0] === grid [0][2]) {
+      //     return grid [2][0]
+         }
+
+         for (var i = 0; i < 3; i++) {
+           for (var j = 0; j < 3; j++){
+             if (grid[i][j] === ' ') {
+               return false;
+             }
+           }
+         }
+       return null;
+  }
+    function moveAi() {
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++){
+          if (grid[i][j] === ' ') {
+            return {
+              i: i,
+              j: j
+            }
+          }
+        }
+      }
+      return null;
+      }
+
+      $('.col').click(function () {
+        $(this).html(userToken)
+        const i = $(this).data('i')
+        const j = $(this).data('j')
+        grid[i][j] = userToken
+
+        let gameState = isGameOver()
+        if (gameState) {
+          alert('game over: ' + gameState)
+        } else {
+          const move = moveAi()
+          grid[move.i][move.j] = oppToken
+          $('.col[data-i=' + move.i + '] [data-j' + move.j + ']').html(oppToken)
+        }
+
+        gameState = isGameOver()
+        if (gameState) {
+          alert('game over: ' + gameState)
+        }
+      }
+        //  $('#testToggle').close(function () {
+        //    $('.container').toggle()
+      )
+    }
+  }
 })
