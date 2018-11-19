10182bc0fde8c17fbb40c5b33694f717805739df
michael-rai <michaelrai.prgmr@gmail.com>
2018-11-18 20:36:25 -0500

    issues with switching to master

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,96 +2,123 @@
 
 // const events = require('./events')
 
-const userToken = 'X'
-const oppToken = 'Y'
+// const userToken = 'X'
+// const oppToken = 'Y'
 
+// Place an item.
 $(() => {
-  console.log('js is running')
-  //  $('#signUpFormModal').on('submit', events.onSignUp)
-  // $('#loginInFormModal').on('submit', events.onSignIn)
+  let playerChoice = "X";
+  let playerScore = 0;
+  let computerChoice = "O";
+  let computerScore = 0;
+  let playersTurn = true;
+  let computerSpot = '.cell-' + getRandomInt(0,2) + '-' + getRandomInt(0,2);
 
-  // the following code can be attributed to Cody Seibert via Youtube at https://www.youtube.com/watch?v=ra2_rKV0mDE
-  const grid = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ]
-  const isGameOver = function () {
-    // check horizontal
-    for (let i = 0; i < 3; i++) {
-      if (grid[i][0] !== ' ' &&
-         grid[i][0] === grid[i][1] &&
-         grid[i][0] === grid[i][2]) {
-        return grid[i][0]
-} else
-      // check vertical
-       for (let j = 0; j < 3; j++) {
-        if (grid[0][j] !== ' ' &&
-          grid[0][j] === grid [1][j] &&
-          grid[0][j] === grid [2][j]) {
-           return grid [0][j]
-          }
-       } elsif
-         // check diagonal-top left bottom right
-           (grid[0][0] !== ' ' &&
-           grid[0][0] === grid [1][1] &&
-           grid[0][0] === grid [2][2])
-            return grid [0][0]
-          } elseif
-          //check diagonal-bottom left top right
-         (grid[2][0] !== ' ' &&
-          grid[2][0] === grid [1][1] &&
-          grid[2][0] === grid [0][2])
-           return grid [2][0]
-         }
+  $(".board-cell").click(function(){
+  		  	//figure out loop logic for if a spot is taken.
+  		  	if (playersTurn) {
+  		  		if ($(this).html() != "&nbsp;") {
+  		  			$('.message-text').text(x + " Trying to cheat? You loose a turn.");
+  		  		} else if ($(this).html() == computerChoice){
+  		  			$('.message-text').text(x + " Trying to cheat? You loose a turn.");
+  		  		} else {
+  			 		$(this).text(playerChoice);
+  			 		playersTurn = false;
+  		  		};
+  		 	}
+  		 	// check the player
+  		    if (checkWin(playerChoice) == true) {
+  		    	playerScore += 1;
+  		    	$('.playerScore').html(playerScore);
+  		    	resetBoard();
+  		    };
+  		    if (checkTie() == true) {
+  		    	resetBoard();
+  		    };
+  		    //run the computer
+  	 		while ($(computerSpot).html() != "&nbsp;") {
+  				computerSpot = '.cell-' + getRandomInt(0,2) + '-' + getRandomInt(0,2);
+  	 		}
+  	 		setTimeout(function(){
+  	 			$(computerSpot).text(computerChoice);
+  	 			playersTurn = true;
+  	 		//check the computer
+  		    if (checkWin(computerChoice) == true) {
+  		    	computerScore += 1;
+  		    	$('.computerScore').html(computerScore);
+  		    	resetBoard();
+  		    };
+  		    if (checkTie() == true) {
+  		    	resetBoard();
+  		    };
+  	 		},300);
 
-         for (let i = 0; i < 3; i++) {
-           for (let j = 0; j < 3; j++){
-             if (grid[i][j] === ' ') {
-               return false;
-             }
-           }
-         }
-       return null
+  		// x will be whoevers hit spots you are checking
+  	    function checkWin(x){
+  	    	var win = false;
+  	    	//check horizontal
+  	    	if ($('.cell-0-0').text() == x
+  	    	 && $('.cell-0-1').text() == x
+  	    	 && $('.cell-0-2').text() == x ||
+  	    	 	$('.cell-1-0').text() == x
+  	    	 && $('.cell-1-1').text() == x
+  	    	 && $('.cell-1-2').text() == x ||
+  	    	 	$('.cell-2-0').text() == x
+  	    	 && $('.cell-2-1').text() == x
+  	    	 && $('.cell-2-2').text() == x) {
+  	    	 	$('.message-text').text(x + " Won last game");
+  	    		win = true;
+  	    	};
+  	    	//check vertical
+  	    	if ($('.cell-0-0').text() == x
+  	    	 && $('.cell-1-0').text() == x
+  	    	 && $('.cell-2-0').text() == x ||
+  	    	 	$('.cell-0-1').text() == x
+  	    	 && $('.cell-1-1').text() == x
+  	    	 && $('.cell-2-1').text() == x ||
+  	    	 	$('.cell-0-2').text() == x
+  	    	 && $('.cell-1-2').text() == x
+  	    	 && $('.cell-2-2').text() == x) {
+  	    	 	$('.message-text').text(x + " Won last game");
+  	    		win = true;
+  	    	};
+  	    	//check diagnal
+  	    	if ($('.cell-0-0').text() == x
+  	    	 && $('.cell-1-1').text() == x
+  	    	 && $('.cell-2-2').text() == x ||
+  	    	 	$('.cell-0-2').text() == x
+  	    	 && $('.cell-1-1').text() == x
+  	    	 && $('.cell-2-0').text() == x) {
+  	    	 	$('.message-text').text(x + " Won last game");
+  	    		win = true;
+  	    	};
+  	    	//return
+  	    	return win;
+  	    }
 
+  	    //clear the board
+  	    function resetBoard(){
+  	    	$('.board-cell').html('&nbsp;');
+  	    }
 
-    const moveAi = function() {
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++){
-          if (grid[i][j] === ' ') {
-            return {
-              i: i,
-              j: j
-            }
-          }
+  	    function checkTie(){
+  	    	var tie = false;
+  	    	if ($('.cell-0-0').html() != "&nbsp;"
+  	    	&&  $('.cell-0-1').html() != "&nbsp;"
+  	    	&&  $('.cell-0-2').html() != "&nbsp;"
+  	    	&&  $('.cell-1-0').html() != "&nbsp;"
+  	    	&&  $('.cell-1-1').html() != "&nbsp;"
+  	    	&&  $('.cell-1-2').html() != "&nbsp;"
+  	    	&&  $('.cell-2-0').html() != "&nbsp;"
+  	    	&&  $('.cell-2-1').html() != "&nbsp;"
+  	    	&&  $('.cell-2-2').html() != "&nbsp;") {
+  		    	$('.message-text').text("It's A Tie");
+  	    		tie = true;
+  	    	};
+  	    	return tie;
+  	    }
+  	    //computers random number
+  		function getRandomInt(min, max) {
+      		return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-      }
-      return null;
-      }
-      // actually click function below:
-      $('.col').click(function () {
-        $(this).html(userToken)
-        const i = $(this).data('i')
-        const j = $(this).data('j')
-        grid[i][j] = userToken
-
-        let gameState = isGameOver()
-        if (gameState) {
-          alert('game over: ' + gameState)
-        } else {
-          const move = moveAi()
-          grid[move.i][move.j] = oppToken
-          $('.col[data-i=' + move.i + '] [data-j' + move.j + ']').html(oppToken)
-        }
-
-        gameState = isGameOver()
-        if (gameState) {
-          alert('game over: ' + gameState)
-        }
-      }
-        //  $('#testToggle').close(function () {
-        //    $('.container').toggle()
-      )
     }
-  }
-)
